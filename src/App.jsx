d0c9a14e5a884f436e4a773c05cc175c3d2ee86b
Keaton-Nordby <keatonnordby4@gmail.com>
2024-12-21 10:28:06 -0600

    Initial commit with partial project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './sections/Header.jsx'
+import Hero from './sections/Hero.jsx'
+const App = () => {
+    return (
+        <main className="overflow-hidden">
+            <Header />
+            <Hero />
+
+        </main>
+    )
+}
+export default App

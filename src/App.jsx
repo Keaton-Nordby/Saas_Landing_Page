182b32bbf60f7a33ab62efd97fe2a1c932ffc789
Keaton-Nordby <keatonnordby4@gmail.com>
2024-12-21 15:31:18 -0600

    Almost done: Added new components and updated existing sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
-import Header from './sections/Header.jsx'
-import Hero from './sections/Hero.jsx'
+import Header from './sections/Header.jsx';
+import Hero from './sections/Hero.jsx';
+import Features from './sections/Features.jsx';
+import Pricing from './sections/Pricing.jsx';
+import Faq from './sections/Faq.jsx';
+import Testimonials from "./sections/Testimonials.jsx";
+
 const App = () => {
     return (
         <main className="overflow-hidden">
             <Header />
             <Hero />
+            <Features />
+            <Pricing />
+            <Faq />
+            <Testimonials />
+
 
         </main>
     )
---
+++ src/components/TestimonialItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const TestimonialItem = () => {
+    return (
+        <div>TestimonialItem</div>
+    )
+}
+export default TestimonialItem
